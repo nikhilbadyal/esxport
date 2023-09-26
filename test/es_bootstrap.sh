fb452071f56a6ace951800f641d7502db52b802f
Nikhil Badyal <nikhill773384@gmail.com>
2023-09-26 19:17:18 +0530

    ✅ Remove existing containers

--- test/es_bootstrap.sh
+++ test/es_bootstrap.sh
@@ -12,6 +12,17 @@ fi
 
 MAJOR_VERSION="$(echo "${STACK_VERSION}" | cut -c 1)"
 network_name=elastic
+stop_and_remove_containers() {
+  existing_containers=$(docker ps -a --filter "network=$network_name" --filter "name=es*" -q)
+
+  if [ -n "$existing_containers" ]; then
+    echo "Stopping and removing existing Elasticsearch containers in network $network_name:"
+    docker kill "$existing_containers"
+    docker rm --force "$existing_containers"
+  fi
+}
+
+stop_and_remove_containers
 if ! docker network inspect "$network_name" &>/dev/null; then
   docker network create "$network_name"
   echo "Created network: $network_name"
@@ -133,7 +144,7 @@ do
     fi
   fi
 done
-
+sleep 10
 if [ "x${MAJOR_VERSION}" == 'x8' ] && [ "${SECURITY_ENABLED}" == 'true' ]; then
   docker run \
     --network elastic \
