4242a54f1f1baaf077e89bc7373961411098281a
Nikhil Badyal <nikhill773384@gmail.com>
2025-04-19 09:51:58 +0530

    ⬆️ Bump to 9.0.0

--- esxport/__init__.py
+++ esxport/__init__.py
@@ -3,5 +3,5 @@
 from esxport.click_opt.cli_options import CliOptions
 from esxport.esxport import EsXport
 
-__version__ = "8.16.0"
+__version__ = "9.0.0"
 __all__ = ["CliOptions", "EsXport", "__version__"]
