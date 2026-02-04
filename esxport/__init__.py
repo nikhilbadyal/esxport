da311aa90269b5a996fd6cb0dfb22031b921bcbd
Nikhil Badyal <59223300+nikhilbadyal@users.noreply.github.com>
2026-02-04 06:51:29 +0530

    ⬆️ Bump to 9.3.0 (#123)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- esxport/__init__.py
+++ esxport/__init__.py
@@ -3,5 +3,5 @@
 from esxport.click_opt.cli_options import CliOptions
 from esxport.esxport import EsXport
 
-__version__ = "9.2.1"
+__version__ = "9.3.0"
 __all__ = ["CliOptions", "EsXport", "__version__"]
